de58a19a5d735f42a263c03192e93c3e2f7afb30
chou0827 <35863674+chou0827@users.noreply.github.com>
2018-02-10 14:20:46 -0800

    Create circle-grid.js

---
+++ circle-grid.js
@@ -0,0 +1,11 @@
+background(0);
+
+function draw () {
+
+	for (var x = 10; x < 340; x = x + 20) {
+  	for (var y = 10; y < 340; y = y + 20) {
+  		ellipse(x, y, 17, 17);
+  		fill(255);
+}
+  }
+  }
