ebcb1577fe1923b82b0c5b77c0343629f5bbc4b2
chou0827 <35863674+chou0827@users.noreply.github.com>
2018-02-11 18:39:35 -0800

    Update patch.js

--- patch.js
+++ patch.js
@@ -1,43 +1,26 @@
-function NickPatch(x, y) {
-  noFill();
-  stroke(238);
-  rect(x, y, 100, 100);
-  
-  // blocky J
-  fill(238);
-  noStroke();
-  rect(x+20, y+20, 20, 60);
-  rect(x+60, y+20, 20, 60);
-  rect(x+40, y+40, 20, 20);
-  // overlay of lines
-  stroke(0);
-  var lines = 3;
-  while (lines < 50) {
-    line(x+lines, y, x, y+lines);
-    lines = lines + 5;
-  }
-  stroke(200);
-  while (lines < 100) {
-    line(x+lines, y, x, y+lines);
-    lines = lines + 3;
-  }
-  stroke(255, 127, 0);
-  lines = 0;
-  while (lines < 50) {
-    line(x+100, y+lines, x+lines, y+100);
-    lines = lines + 4;
-  }
-  stroke(0, 255, 255);
-  while (lines < 100) {
-    line(x+100, y+lines, x+lines, y+100);
-    lines = lines + 4;
-  }
-}
-  
-background(255);
-NickPatch(0, 0); // draw patch at upper-left
+background(255, 255, 0);
 
-background(255);
-NickPatch(width-100, height-100); // draw patch at lower-right
+noFill();
+stroke(255,0,10);
+strokeWeight(20);
+rect(150, 150, 40, 40);
+
+
+ellipse(170, 170, 100, 100)
+ellipse(170, 170, 150, 150)
+ellipse(170, 170, 200, 200)
+ellipse(170, 170, 250, 250)
+
+stroke(255,0,100);
+function draw () {
+
+	for (var x = 10; x < 340; x = x + 20) {
+  	  ellipse(330,x, 10, 10);
+  		ellipse(10, x, 10, 10);
+  		ellipse(x,10,10,10);
+      ellipse(x,340,10,10);  
+    fill(255);
+
+    }}
  
  
